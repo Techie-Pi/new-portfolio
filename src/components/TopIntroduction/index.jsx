14aa58993ee2994268bae9072bb26d93b502cccf
TechiePi <techie@techiepi.dev>
2022-01-12 14:43:27 +0100

    fix: do noy open project on a blank page

--- src/components/TopIntroduction/index.jsx
+++ src/components/TopIntroduction/index.jsx
@@ -10,7 +10,7 @@ function TopIntroduction() {
                     Hi! I am a 15 year old girl that started programming since I can remember.
                 </p>
                 <p className={pClassName}>
-                    Right now I am working on some interesting and innovative projects, you can see some of them at <Link href="/projects">Projects</Link>
+                    Right now I am working on some interesting and innovative projects, you can see some of them at <Link blank={false} href="/projects">Projects</Link>
                 </p>
                 <p className={pClassName}>
                     I love cryptography and cybersecurity. In my free time I like to participate and organize activities like ARGs and similar things.
