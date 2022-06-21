e2ec34f8a63e7d35b19ce80ef4b7e04425a1072a
TechiePi <techie@techiepi.dev>
2022-06-22 00:14:41 +0200

    improve project page title

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -3,8 +3,7 @@ import React from "react";
 function Title({ children }) {
     return (
             <h3
-                className="tracking-wide text-3xl text-center font-bold my-4 dark:text-gray-100"
-                style={{ fontWeight: 300, fontFamily: "Lato, sans-serif" }}
+                className="tracking-wide text-3xl text-center font-bold my-4 dark:text-gray-100 font-normal font-['Lato']"
             >
                 {children}
             </h3>
