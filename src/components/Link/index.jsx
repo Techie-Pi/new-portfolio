2b6840a876d84fce124be36f0e8dc4a86f8f355d
TechiePi <techie@techiepi.dev>
2022-06-22 00:18:01 +0200

    improve links

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -4,7 +4,7 @@ function Link({ href, children, blank=true, className }) {
     return (
             <a
                href={href}
-               className={"text-amber-50 uppercase font-semibold tracking-wider transition-all duration-[350ms] ease-in hover:dark:text-amber-100 font-['Lato']" + " " + className}
+               className={"text-amber-50 underline transition-all duration-[350ms] ease-in hover:dark:text-amber-200" + " " + className}
                rel="noopener noreferrer"
                target={blank ? "_blank" : "_self"}
             >
